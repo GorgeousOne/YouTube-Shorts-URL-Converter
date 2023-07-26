c6b75c419dc8b9b05acc64d2d980da304578cfb4
IEatSushi <2klaus.4peter@gmail.com>
2023-07-26 23:41:44 +0200

    add auto focus to search when typing

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ searchButton = document.getElementById("search-icon");
 search = document.getElementById("search");
 errorMessage = document.getElementById("error-message");
 
+//focus the search bar as soon as keyboard is typed
+window.addEventListener("keydown", function () {
+	search.focus();
+});
+
+//trigger url conversion on search button press
 searchButton.addEventListener("click", function (event) {
 	openNormalVideo(search.value);
 });
 
+//trigger url conversion on ENTER press
 search.addEventListener("keyup", function (event) {
 	if (event.key === "Enter") {
 		searchButton.click();
@@ -18,10 +25,10 @@ search.addEventListener("input", function (event) {
 	}
 });
 
+//open tab with altered url if possible
 function openNormalVideo(url) {
 	if (url.includes("youtube.com/shorts/")) {
 		let normalUrl = url.replace("shorts/", "watch?v=");
-		console.log(normalUrl);
 		window.open(normalUrl, "_blank").focus();
 	} else if (url !== "") {
 		errorMessage.innerText = "This URL does not contain \"youtube.com/shorts/...\"";
